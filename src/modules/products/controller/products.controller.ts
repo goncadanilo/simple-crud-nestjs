58b8547e036872f8d47a65905d13542bb26c96d2
Danilo Gonçalves <gonca.danilo@gmail.com>
2020-09-19 17:50:18 -0300

    feat: create product

--- src/modules/products/controller/products.controller.ts
+++ src/modules/products/controller/products.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { CreateProductDto } from '../dtos/create-product.dto';
+import { Products } from '../entity/products.entity';
 import { ProductsService } from '../service/products.service';
 
 @Controller('products')
 export class ProductsController {
   constructor(private productsService: ProductsService) {}
+
+  @Post()
+  @HttpCode(201)
+  async createProduct(@Body() data: CreateProductDto): Promise<Products> {
+    return await this.productsService.createProduct(data);
+  }
 }
