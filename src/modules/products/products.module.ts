336278c865f8dd252fb602a28de0056bb0d92b5f
Danilo Gonçalves <gonca.danilo@gmail.com>
2020-09-19 17:13:23 -0300

    fix: set products entity in the products module imports

--- src/modules/products/products.module.ts
+++ src/modules/products/products.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Products } from './entity/products.entity';
 import { ProductsService } from './service/products.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Products])],
   providers: [ProductsService],
 })
 export class ProductsModule {}
