3c3cc0c965ac702e2c0ab189d65fc18a8a8929b2
Danilo Gonçalves <gonca.danilo@gmail.com>
2020-09-19 13:32:21 -0300

    feat: set ConfigModule in the AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
 })
 export class AppModule {}
