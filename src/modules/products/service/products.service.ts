8e7a40a824bbcf17b3255c19aa79d8ca848c2528
Danilo Gonçalves <gonca.danilo@gmail.com>
2020-09-19 16:42:56 -0300

    feat: should create a product

--- src/modules/products/service/products.service.ts
+++ src/modules/products/service/products.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreateProductDto } from '../dtos/create-product.dto';
 import { Products } from '../entity/products.entity';
 
 @Injectable()
@@ -9,4 +10,9 @@ export class ProductsService {
     @InjectRepository(Products)
     private repository: Repository<Products>,
   ) {}
+
+  async createProduct(data: CreateProductDto): Promise<Products> {
+    const product = this.repository.create(data);
+    return await this.repository.save(product);
+  }
 }
